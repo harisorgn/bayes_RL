4f44f0847cc4da968b440ae84d9cf95792a3cd96
haris <organtzh@gmail.com>
2021-05-25 09:42:15 +0100

    started work on pluto notebooks for visuals & LOO

--- main.jl
+++ main.jl
@@ -6,16 +6,7 @@ using ReverseDiff
 using Serialization
 using FillArrays
 
-struct ABT_t
-	n_sessions::Int64
-	n_subjects::Int64
-	n_groups::Int64
-	avail_actions_m::Matrix{Array{Int64,1}}
-	group_m::Matrix{Int64}
-	R_m::Matrix{Array{Float64,1}}
-	trial_m::Matrix{Int64}
-end
-
+include("task_types.jl")
 include("read_abt.jl")
 include("lapse.jl")
 include("softmax.jl")
---
+++ task_types.jl
@@ -0,0 +1,10 @@
+
+struct ABT_t
+	n_sessions::Int64
+	n_subjects::Int64
+	n_groups::Int64
+	avail_actions_m::Matrix{Array{Int64,1}}
+	group_m::Matrix{Int64}
+	R_m::Matrix{Array{Float64,1}}
+	trial_m::Matrix{Int64}
+end
